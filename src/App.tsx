45de06deca9451ba45308a640dcc200f81364fae
Anuranjan Srivastav <toanuranjans@gmail.com>
2025-11-07 07:49:24 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,26 @@ import React, { useMemo } from "react";
 import "./app.css";
 
 const App = () => {
-    const [counter, setCounter] = React.useState(0);
-    const resource = useMemo(() => {
-        if (typeof window === 'undefined') {
-            return null;
-        }
+  const [counter, setCounter] = React.useState(0);
+  const resource = useMemo(() => {
+    if (typeof window === 'undefined') {
+      return null;
+    }
 
-        return window.__APP_RESOURCE__ ?? null;
-    }, []);
+    return window.__APP_RESOURCE__ ?? null;
+  }, []);
 
-    return (
-        <div>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-            <h1>App 10 {counter}</h1>
-            <button onClick={() => setCounter(counter + 1)}>Count: {counter}</button>
-            <pre>{JSON.stringify(resource, null, 2)}</pre>
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <h1>App 10 {counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>Count: {counter}</button>
+      <pre>{JSON.stringify(resource, null, 2)}</pre>
 
-        </div>
-    );
+    </div>
+  );
 };
 
 export default App;
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,12 +1,9 @@
 declare global {
-    interface Window {
-        __APP_RESOURCE__?: unknown;
-    }
+  interface Window {
+    __APP_RESOURCE__?: unknown;
+  }
 }
 
-declare module '*.css';
-declare module '*.less';
-declare module '*.scss';
-declare module '*.sass';
+
 
 export { };
